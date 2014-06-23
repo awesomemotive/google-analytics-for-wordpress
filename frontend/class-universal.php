a456c34c03139e1aea91a9a4089e8468d2da2b2c
Peter van Wilderen <peter@yoast.com>
2014-06-23 13:38:19 +0200

    Added comments and basic wp head functionality for ga.js

---
+++ frontend/class-universal.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is the frontend class for the GA Universal code
+ */
+
+if( !class_exists('Yoast_GA_Universal') ){
+
+	class Yoast_GA_Universal extends Yoast_GA_Frontend {
+
+		public function __construct(){
+
+		}
+
+	}
+
+	$Yoast_GA_Universal 	=	new Yoast_GA_Universal;
+}
