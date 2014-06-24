0908b40e5d67931eb0dbb163d158d43e3e4876c5
Peter van Wilderen <peter@yoast.com>
2014-06-24 09:48:07 +0200

    Enqueue styles and scripts

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -1,1 +1,26 @@
-Settings
+
+<h2 id="yoast_ga_title"><?php echo __('Yoast Google Analytics: Settings', ''); ?></h2>
+
+<h2 class="nav-tab-wrapper" id="ga-tabs">
+	<a class="nav-tab" id="general-tab" href="#top#general"><?php _e( 'General', 'wordpress-seo' );?></a>
+	<a class="nav-tab" id="home-tab" href="#top#advanced"><?php _e( 'Advanced', 'wordpress-seo' );?></a>
+	<a class="nav-tab" id="post_types-tab" href="#top#debugmode"><?php _e( 'Debug mode', 'wordpress-seo' );?></a>
+</h2>
+
+<div class="tabwrapper">
+	<div id="general" class="gatab">
+<?php
+echo '<h2>' . __( 'Title settings', 'wordpress-seo' ) . '</h2>';
+?>
+	</div>
+	<div id="advanced" class="gatab">
+		<?php
+		echo '<h2>' . __( 'Title settings', 'wordpress-seo' ) . '</h2>';
+		?>
+	</div>
+	<div id="debugmode" class="gatab">
+		<?php
+		echo '<h2>' . __( 'Title settings', 'wordpress-seo' ) . '</h2>';
+		?>
+	</div>
+</div>
---
+++ js/yoast_ga_admin.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+
+	jQuery('#ga-tabs').find('a').click(function() {
+		jQuery('#ga-tabs').find('a').removeClass('nav-tab-active');
+		jQuery('.gatab').removeClass('active');
+
+		var id = jQuery(this).attr('id').replace('-tab','');
+		jQuery('#' + id).addClass('active');
+		jQuery(this).addClass('nav-tab-active');
+	});
+
+	// init
+	var active_tab = window.location.hash.replace('#top#','');
+
+	// default to first tab
+	if ( active_tab == '' || active_tab == '#_=_') {
+		active_tab = jQuery('.gatab').attr('id');
+	}
+
+	jQuery('#' + active_tab).addClass('active');
+	jQuery('#' + active_tab + '-tab').addClass('nav-tab-active');
+
+});
