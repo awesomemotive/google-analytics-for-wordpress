d2566bb901f7ec5d529a1e0072798b211b9a393d
Peter van Wilderen <peter@yoast.com>
2014-06-25 12:19:55 +0200

    Created other basic page content

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -1,1 +1,10 @@
-Dashboard
+<?php
+global $yoast_ga_admin;
+
+echo $yoast_ga_admin->content_head();
+?>
+	<h2 id="yoast_ga_title"><?php echo __( 'Yoast Google Analytics: Dashboard', 'google-analytics-for-wordpress' ); ?></h2>
+
+<?php
+echo $yoast_ga_admin->content_footer();
+?>
--- admin/pages/extensions.php
+++ admin/pages/extensions.php
@@ -1,1 +1,10 @@
-Extensions
+<?php
+global $yoast_ga_admin;
+
+echo $yoast_ga_admin->content_head();
+?>
+	<h2 id="yoast_ga_title"><?php echo __( 'Yoast Google Analytics: Extensions', 'google-analytics-for-wordpress' ); ?></h2>
+
+<?php
+echo $yoast_ga_admin->content_footer();
+?>
