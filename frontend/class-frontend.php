52d5768cc079c43aa742577728f4a103c77e0961
Peter van Wilderen <peter@yoast.com>
2014-07-07 15:08:50 +0200

    Added options and inserted some old code

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -16,6 +16,31 @@ if( !class_exists('Yoast_GA_Frontend') ){
 		public function get_options(){
 			return self::$options;
 		}
+
+		/**
+		 * Parse the domain
+		 * @param $uri
+		 *
+		 * @return array|bool
+		 */
+		public function yoast_ga_get_domain( $uri ) {
+			$hostPattern     = "/^(http:\/\/)?([^\/]+)/i";
+			$domainPatternUS = "/[^\.\/]+\.[^\.\/]+$/";
+			$domainPatternUK = "/[^\.\/]+\.[^\.\/]+\.[^\.\/]+$/";
+
+			preg_match( $hostPattern, $uri, $matches );
+			$host = $matches[2];
+			if ( preg_match( "/.*\..*\..*\..*$/", $host ) )
+				preg_match( $domainPatternUK, $host, $matches );
+			else
+				preg_match( $domainPatternUS, $host, $matches );
+
+			if ( isset( $matches[0] ) ) {
+				return array( "domain" => $matches[0], "host" => $host );
+			} else {
+				return false;
+			}
+		}
 	}
 
 	global $yoast_ga_frontend;
