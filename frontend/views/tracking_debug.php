d1bc18dae25de1cc3ee81950b035dbfeabeea2ea
Peter van Wilderen <peter@yoast.com>
2014-07-25 13:29:13 +0200

    Debug mode feature added

---
+++ frontend/views/tracking_debug.php
@@ -0,0 +1,3 @@
+<!-- This site uses the Yoast Google Analytics plugin v<?php echo GAWP_VERSION; ?> - https://yoast.com/wordpress/plugins/google-analytics/ -->
+<!-- @Webmaster, normally you will find the Google Analytics tracking code here, but you have enabled the Debug Mode. Change that in Analytics -> (Tab) Debug mode and disable Debug Mode to enable the tracking of your site. -->
+<!-- / Yoast Google Analytics -->
