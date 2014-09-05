7f289c1769ba6d5ad9d9cadec6c53b70eb79f368
Peter van Wilderen <peter@yoast.com>
2014-09-05 13:02:08 +0200

    Travis fix for submodules

--- tests/test-class-admin.php
+++ tests/test-class-admin.php
@@ -2,11 +2,21 @@
 
 class Yoast_GA_Admin_Test extends GA_UnitTestCase {
 
-
-	public function test_init_ga(){
-
+	/**
+	 * @covers Yoast_GA_Admin->init_ga()
+	 */
+	public function test_init_ga() {
 		$this->go_to_home();
 		$this->assertTrue( true );
+	}
+
+	/**
+	 * @covers Yoast_GA_Admin->add_aciton_links()
+	 */
+	public function test_add_action_links() {
+		$links = array();
+
+		$this->assertNotNull( $this->add_action_links( $links ) );
 
 	}
 
