2d1732894a453535ca025f5147476466cb016108
Joost de Valk <joost@yoast.com>
2014-09-16 15:56:58 +0200

    Activation check

--- admin/pages/extensions.php
+++ admin/pages/extensions.php
@@ -2,6 +2,16 @@
 global $yoast_ga_admin;
 
 echo $yoast_ga_admin->content_head();
+
+$activated = false;
+if ( class_exists( 'Yoast_Product_GA_eCommerce' ) ) {
+	$product         = new Yoast_Product_GA_eCommerce();
+	$license_manager = new Yoast_Plugin_License_Manager( $product );
+	if ( $license_manager->license_is_valid() ) {
+		$activated = true;
+	}
+}
+
 ?>
 	<h2 id="yoast_ga_title"><?php echo __( 'Yoast Google Analytics: Extensions', 'google-analytics-for-wordpress' ); ?></h2>
 
@@ -14,8 +24,10 @@ echo $yoast_ga_admin->content_head();
 			<p>
 				<?php if ( class_exists( 'Yoast_GA_eCommerce_Tracking' ) ) { ?>
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/ga-ecommerce-edd/#utm_medium=banner&utm_source=gawp-config&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a>
+				<?php } else if ( ! $activated ) { ?>
+					<a href="#top#licenses" class="button-primary">Activate License</a>
 				<?php } else { ?>
-				<button class="button-primary installed">Installed</button>
+					<button class="button-primary installed">Installed</button>
 				<?php } ?>
 			</p>
 		</div>
