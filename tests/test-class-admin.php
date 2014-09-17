efb7925044fd264b93154328617ce61bded2b4a9
Peter van Wilderen <peter@yoast.com>
2014-09-17 11:28:02 +0200

    Test form options for download types and full url

--- tests/test-class-admin.php
+++ tests/test-class-admin.php
@@ -60,6 +60,52 @@ class Yoast_GA_Admin_Test extends GA_UnitTestCase {
 	}
 
 	/**
+	 * Test download types
+	 *
+	 * @covers Yoast_GA_Admin->track_download_types()
+	 */
+	public function test_track_download_types() {
+		$download_types        = $this->class_instance->track_download_types();
+		$download_types_result = is_array( $download_types );
+
+		if ( $download_types_result ) {
+			foreach ( $download_types as $values ) {
+				$this->assertArrayHasKey( 'id', $values );
+				$this->assertArrayHasKey( 'name', $values );
+				if ( isset( $values['name'] ) ) {
+					$this->assertNotEmpty( $values['name'] );
+				}
+			}
+		} else {
+			$this->assertTrue( $download_types_result );
+		}
+
+	}
+
+	/**
+	 * Test track full url option
+	 *
+	 * @covers Yoast_GA_Admin->get_track_full_url()
+	 */
+	public function test_get_track_full_url() {
+		$track_options        = $this->class_instance->get_track_full_url();
+		$track_options_result = is_array( $track_options );
+
+		if ( $track_options ) {
+			foreach ( $track_options as $values ) {
+				$this->assertArrayHasKey( 'id', $values );
+				$this->assertArrayHasKey( 'name', $values );
+				if ( isset( $values['name'] ) ) {
+					$this->assertNotEmpty( $values['name'] );
+				}
+			}
+		} else {
+			$this->assertTrue( $track_options_result );
+		}
+
+	}
+
+	/**
 	 * Create a form, receives the html output
 	 *
 	 * @covers Yoast_GA_Admin->create_form()
