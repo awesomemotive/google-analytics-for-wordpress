a16fbb128b442c5162505fa95f97c30e0f3428f3
omarreiss <omar.reiss@gmail.com>
2014-09-22 12:16:35 +0200

    apply code style

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -77,14 +77,14 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 		 */
 		public function save_settings( $data ) {
 			foreach ( $data as $key => $value ) {
-				$this->options[ $key ] = $value;
+				$this->options[$key] = $value;
 			}
 
 			// Check checkboxes, on a uncheck they won't be posted to this function
 			$defaults = $this->default_ga_values();
-			foreach ( $defaults[ $this->option_prefix ] as $key => $value ) {
-				if ( ! isset( $data[ $key ] ) ) {
-					$this->options[ $key ] = $value;
+			foreach ( $defaults[$this->option_prefix] as $key => $value ) {
+				if ( ! isset( $data[$key] ) ) {
+					$this->options[$key] = $value;
 				}
 			}
 
@@ -135,65 +135,66 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 			add_menu_page( __( 'Yoast Google Analytics:', 'google-analytics-for-wordpress' ) . ' ' . __( 'General Settings', 'google-analytics-for-wordpress' ), __( 'Analytics', 'google-analytics-for-wordpress' ), 'manage_options', 'yst_ga_dashboard', array(
 				$this,
 				'load_page',
-			), $icon_svg, $on_top ? '2.00013467543': '100.00013467543' );
+			), $icon_svg, $on_top ? '2.00013467543' : '100.00013467543' );
 
-			$this->add_submenu_pages();		
+			$this->add_submenu_pages();
 		}
 
 
-    private function prepare_submenu_page ($submenu_name, $font_color) {
-      $menu_title = __(  ucfirst($submenu_name), 'google-analytics-for-wordpress' );
-        if ( !empty($font_color) ){
-          $menu_title = __( '<span style="color:' . $font_color . '">' . $menu_title . '</span>', 'google-analytics-for-wordpress' );
-        }
+		private function prepare_submenu_page( $submenu_name, $font_color ) {
+			$menu_title = __( ucfirst( $submenu_name ), 'google-analytics-for-wordpress' );
+			if ( ! empty( $font_color ) ) {
+				$menu_title = __( '<span style="color:' . $font_color . '">' . $menu_title . '</span>', 'google-analytics-for-wordpress' );
+			}
 
-        $submenu_page = array(
-          'parent_slug' => 'yst_ga_dashboard',
-          'page_title' => __( 'Yoast Google Analytics:', 'google-analytics-for-wordpress' ) . ' ' . __( ucfirst($submenu_name), 'google-analytics-for-wordpress' ),
-          'menu_title' => $menu_title,
-          'capability' => 'manage_options',
-          'menu_slug' => 'yst_ga_' . $submenu_name,
-          'submenu_function' => array( $this, 'load_page' ),
-        );
-        return $submenu_page;
-    }
+			$submenu_page = array(
+				'parent_slug'      => 'yst_ga_dashboard',
+				'page_title'       => __( 'Yoast Google Analytics:', 'google-analytics-for-wordpress' ) . ' ' . __( ucfirst( $submenu_name ), 'google-analytics-for-wordpress' ),
+				'menu_title'       => $menu_title,
+				'capability'       => 'manage_options',
+				'menu_slug'        => 'yst_ga_' . $submenu_name,
+				'submenu_function' => array( $this, 'load_page' ),
+			);
 
-    private function add_submenu_page ($submenu_page) {
-      $page = add_submenu_page( $submenu_page['parent_slug'], $submenu_page['page_title'], $submenu_page['menu_title'], $submenu_page['capability'], $submenu_page['menu_slug'], $submenu_page['submenu_function'] );
-      add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_styles' ) );
-      if ( 'yst_ga_settings' === $submenu_page["menu_slug"] || 'yst_ga_extensions' === $submenu_page["menu_slug"] ) {
-        add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_settings_styles' ) );
-        add_action( 'admin_print_scripts-' . $page, array( $this, 'enqueue_scripts' ) );
-      }
-    }
+			return $submenu_page;
+		}
+
+		private function add_submenu_page( $submenu_page ) {
+			$page = add_submenu_page( $submenu_page['parent_slug'], $submenu_page['page_title'], $submenu_page['menu_title'], $submenu_page['capability'], $submenu_page['menu_slug'], $submenu_page['submenu_function'] );
+			add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_styles' ) );
+			if ( 'yst_ga_settings' === $submenu_page["menu_slug"] || 'yst_ga_extensions' === $submenu_page["menu_slug"] ) {
+				add_action( 'admin_print_styles-' . $page, array( $this, 'enqueue_settings_styles' ) );
+				add_action( 'admin_print_scripts-' . $page, array( $this, 'enqueue_scripts' ) );
+			}
+		}
 
 		/**
 		 * Prepares the array used to build the submenu
 		 *
 		 * @return array
 		 */
-		private function add_submenu_pages () {
-      /**
-       * Array structure:
-       *
-       * array(
-       *   $submenu_name => $font_color,
-       *   ..,
-       *   ..,
-       * )
-       *
-       * $font_color can be left empty.
-       *
-       */
-      $submenu_types = array(
-				"dashboard" => "",
-				"settings" => "",
+		private function add_submenu_pages() {
+			/**
+			 * Array structure:
+			 *
+			 * array(
+			 *   $submenu_name => $font_color,
+			 *   ..,
+			 *   ..,
+			 * )
+			 *
+			 * $font_color can be left empty.
+			 *
+			 */
+			$submenu_types = array(
+				"dashboard"  => "",
+				"settings"   => "",
 				"extensions" => "#f18500",
 			);
 
 			foreach ( $submenu_types as $submenu_name => $font_color ) {
-				$submenu_page = $this->prepare_submenu_page($submenu_name, $font_color);
-        $this->add_submenu_page( $submenu_page );
+				$submenu_page = $this->prepare_submenu_page( $submenu_name, $font_color );
+				$this->add_submenu_page( $submenu_page );
 			}
 		}
 
@@ -208,6 +209,7 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 			if ( ! defined( 'SCRIPT_DEBUG' ) || ! SCRIPT_DEBUG ) {
 				$ext = '.min' . $ext;
 			}
+
 			return $ext;
 		}
 
@@ -315,12 +317,12 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 				$input .= '<label class="ga-form ga-form-' . $type . '-label ga-form-label-left" id="yoast-ga-form-label-' . $type . '-' . $this->form_namespace . '-' . $id . '" />' . $title . ':</label>';
 			}
 
-			if ( $type == 'checkbox' && $this->options[ $name ] == 1 ) {
+			if ( $type == 'checkbox' && $this->options[$name] == 1 ) {
 				$input .= '<input type="' . $type . '" class="ga-form ga-form-checkbox" id="yoast-ga-form-' . $type . '-' . $this->form_namespace . '-' . $id . '" name="' . $name . '" value="1" checked="checked" />';
 			} elseif ( $type == 'checkbox' ) {
 				$input .= '<input type="' . $type . '" class="ga-form ga-form-checkbox" id="yoast-ga-form-' . $type . '-' . $this->form_namespace . '-' . $id . '" name="' . $name . '" value="1" />';
 			} else {
-				$input .= '<input type="' . $type . '" class="ga-form ga-form-' . $type . '" id="yoast-ga-form-' . $type . '-' . $this->form_namespace . '-' . $id . '" name="' . $name . '" value="' . stripslashes( $this->options[ $name ] ) . '" />';
+				$input .= '<input type="' . $type . '" class="ga-form ga-form-' . $type . '" id="yoast-ga-form-' . $type . '-' . $this->form_namespace . '-' . $id . '" name="' . $name . '" value="' . stripslashes( $this->options[$name] ) . '" />';
 			}
 
 			if ( ! is_null( $text_label ) ) {
@@ -333,7 +335,7 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 			if ( ! is_null( $description ) ) {
 				$input .= '<div class="ga-form ga-form-input">';
 				$input .= '<label class="ga-form ga-form-select-label ga-form-label-left" id="yoast-ga-form-description-select-' . $this->form_namespace . '-' . $id . '" />&nbsp;</label>';
-				$input .= '<span class="ga-form ga-form-description">' .  $description . '</span>';
+				$input .= '<span class="ga-form ga-form-description">' . $description . '</span>';
 				$input .= '</div>';
 			}
 
@@ -367,14 +369,14 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 			}
 			if ( count( $values ) >= 1 ) {
 				foreach ( $values as $value ) {
-					if ( is_array( $this->options[ $name ] ) ) {
-						if ( in_array( $value['id'], $this->options[ $name ] ) ) {
+					if ( is_array( $this->options[$name] ) ) {
+						if ( in_array( $value['id'], $this->options[$name] ) ) {
 							$select .= '<option value="' . $value['id'] . '" selected="selected">' . stripslashes( $value['name'] ) . '</option>';
 						} else {
 							$select .= '<option value="' . $value['id'] . '">' . stripslashes( $value['name'] ) . '</option>';
 						}
 					} else {
-						$select .= '<option value="' . $value['id'] . '" ' . selected( $this->options[ $name ], $value['id'], false ) . '>' . stripslashes( $value['name'] ) . '</option>';
+						$select .= '<option value="' . $value['id'] . '" ' . selected( $this->options[$name], $value['id'], false ) . '>' . stripslashes( $value['name'] ) . '</option>';
 					}
 				}
 			}
@@ -408,7 +410,7 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 			if ( ! is_null( $title ) ) {
 				$text .= '<label class="ga-form ga-form-select-label ga-form-label-left" id="yoast-ga-form-label-select-' . $this->form_namespace . '-' . $id . '" />' . __( $title, 'google-analytics-for-wordpress' ) . ':</label>';
 			}
-			$text .= '<textarea rows="5" cols="60" name="' . $name . '" id="yoast-ga-form-textarea-' . $this->form_namespace . '-' . $id . '">' . stripslashes( $this->options[ $name ] ) . '</textarea>';
+			$text .= '<textarea rows="5" cols="60" name="' . $name . '" id="yoast-ga-form-textarea-' . $this->form_namespace . '-' . $id . '">' . stripslashes( $this->options[$name] ) . '</textarea>';
 			$text .= '</div>';
 
 			// If we get a description, append it to this select field in a new row
@@ -606,8 +608,8 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 		public function get_userroles() {
 			global $wp_roles;
 
-			$all_roles      = $wp_roles->roles;
-			$roles          = array();
+			$all_roles = $wp_roles->roles;
+			$roles     = array();
 
 			/**
 			 * Filter: 'editable_roles' - Allows filtering of the roles shown within the plugin (and elsewhere in WP as it's a WP filter)
