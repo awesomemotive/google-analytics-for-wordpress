bcd70a62e1670516246244d67ef7f3dc505342e3
Andy Meerwaldt <andy@yoast.com>
2014-10-17 14:01:14 +0200

    Changed ga into __gaTracker

--- frontend/views/tracking-universal.php
+++ frontend/views/tracking-universal.php
@@ -3,14 +3,14 @@
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	})(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
 
 <?php
 	// List the GA elements from the class-ga-js.php
 	if ( count( $gaq_push ) >= 1 ) {
 		foreach ( $gaq_push as $item ) {
 			if ( ! is_array( $item ) ) {
-				echo '	ga('.$item.");\n";
+				echo '	__gaTracker('.$item.");\n";
 			}
 			elseif ( isset( $item['value'] ) ) {
 				echo '	'.$item['value'] . "\n";
