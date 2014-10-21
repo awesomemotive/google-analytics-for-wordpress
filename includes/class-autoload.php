bcb150d5552ec41d3533ba0b518850bbc8dae69b
Andy Meerwaldt <andy@yoast.com>
2014-10-21 15:50:28 +0200

    Adding autoloader which handles loading of class-files

---
+++ includes/class-autoload.php
@@ -0,0 +1,49 @@
+<?php
+
+if ( ! class_exists( 'Yoast_GA_Autoload' ) ) {
+
+	class Yoast_GA_Autoload {
+
+		private static $classes = null;
+
+		public static function autoload( $class ) {
+
+			$include_path = dirname( GAWP_FILE );
+
+			if ( self::$classes === null ) {
+
+				self::$classes = array(
+					'yoast_ga_options'             => 'includes/class-options',
+
+					// Frontend classes
+					'yoast_ga_frontend'  		   => 'frontend/class-frontend',
+					'yoast_ga_universal' 		   => 'frontend/class-universal',
+				 	'yoast_ga_js'                  => 'frontend/class-ga-js',
+
+					// Admin classes
+					'yoast_ga_admin'			   => 'admin/class-admin',
+					'yoast_google_analytics'       => 'admin/class-google-analytics',
+					'yoast_ga_admin_ga_js'   	   => 'admin/class-admin-ga-js',
+
+					// License manager
+					'yoast_license_manager'        => 'admin/license-manager/class-license-manager',
+					'yoast_plugin_license_manager' => 'admin/license-manager/class-plugin-license-manager',
+					'yoast_product'                => 'admin/license-manager/class-product'
+				);
+			}
+
+			$class_name = strtolower( $class );
+			if ( isset( self::$classes[$class_name] ) ) {
+				require_once $include_path . '/' . self::$classes[$class_name] . '.php';
+			}
+
+		}
+
+
+	}
+
+	// register class autoloader
+	spl_autoload_register( array( 'Yoast_GA_Autoload', 'autoload' ) );
+
+}
+
