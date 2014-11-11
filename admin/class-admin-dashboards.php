bf23175e80629db2a6c65cf2473e555260495aff
Peter van Wilderen <peter@yoast.com>
2014-11-11 15:44:35 +0100

    New classes for dashboards

---
+++ admin/class-admin-dashboards.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( ! class_exists( 'Yoast_GA_Dashboards' ) ) {
+
+	class Yoast_GA_Dashboards extends Yoast_GA_Admin {
+
+		/**
+		 * Construct on the dashboards class for GA
+		 */
+		public function __construct(){
+
+		}
+
+	}
+
+}
