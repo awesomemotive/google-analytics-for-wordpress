83290f099b8dfb214dd096c2234d573006d7d1de
Joost de Valk <joost@yoast.com>
2014-11-12 14:57:30 +0100

    Fix untranslateable strings, update .pot Remove Joost's avatar

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -6,12 +6,7 @@ echo $yoast_ga_admin->content_head();
 	<h2 id="yoast_ga_title"><?php echo __( 'Yoast Google Analytics: Dashboard', 'google-analytics-for-wordpress' ); ?></h2>
 
 	<div id="ga-promote">
-		<p>This feature
-			<a href="https://yoast.com/google-analytics-5/#utm_medium=textlink&utm_source=gawp-config&utm_campaign=wpgaplugin">will be coming soon</a>. For now, you can
-			<a href="https://yoast.com/cat/analytics/#utm_medium=textlink&utm_source=gawp-config&utm_campaign=wpgaplugin">read our posts on Analytics</a>, or, of course,
-			<a href="http://www.google.com/analytics/">log into Google Analytics</a> yourself.</p>
-
-		<p align="center"><img src="<?php echo GAWP_URL . 'img/'; ?>yoast_avatar_joost.png" width="250"></p>
+		<p><?php printf( __( 'This feature %1$swill be coming soon%2$s. For now, you can %3$sread our posts on Analytics%2$s, or of course, %4$slog into Google Analytics%2$s yourself.', 'google-analytics-for-wordpress' ), '<a href="https://yoast.com/google-analytics-5/#utm_medium=textlink&utm_source=gawp-config&utm_campaign=wpgaplugin">', '</a>', '<a href="https://yoast.com/cat/analytics/#utm_medium=textlink&utm_source=gawp-config&utm_campaign=wpgaplugin">', '<a href="http://www.google.com/analytics/">' ); ?>
 	</div>
 
 <?php
