74e3d3c7fb2f51433e293b234a46f59f78ebc74e
Joost de Valk <joost@yoast.com>
2014-11-12 14:57:30 +0100

    Fix untranslateable strings, update .pot Remove Joost's avatar

--- admin/views/content-footer.php
+++ admin/views/content-footer.php
@@ -5,7 +5,7 @@
 			<img src="<?php echo $item['banner']; ?>" alt="<?php echo $item['title']; ?>" class="yoast-banner" border="0" width="250" />
 	</a></p>
 	<?php endforeach; ?>
-	Remove these ads? <br />
-	<a href="https://yoast.com/wordpress/plugins/google-analytics/#utm_medium=text-link&utm_source=gawp-config&utm_campaign=wpgaplugin" target="_blank">Upgrade to Google Analytics By Yoast Premium »</a>
+	<?php _e( 'Remove these ads?', 'google-analytics-for-wordpress' ); ?><br />
+	<a href="https://yoast.com/wordpress/plugins/google-analytics/#utm_medium=text-link&utm_source=gawp-config&utm_campaign=wpgaplugin" target="_blank"><?php _e( 'Upgrade to Google Analytics By Yoast Premium »', 'google-analytics-for-wordpress' ); ?></a>
 </div>
 </div>
