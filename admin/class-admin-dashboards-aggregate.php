be5569a3215fbd7f230235e98a9c8275b09d512d
Peter van Wilderen <peter@yoast.com>
2014-11-13 11:28:12 +0100

    Added data class with

--- admin/class-admin-dashboards-aggregate.php
+++ admin/class-admin-dashboards-aggregate.php
@@ -12,6 +12,15 @@ if ( ! class_exists( 'Yoast_GA_Dashboards_Aggregrate' ) ) {
 		public function __construct(){
 
 			$this->api = Yoast_Api_Libs::load_api_libraries( array( 'oauth' ) );
+
+			add_action('shutdown', array($this, 'aggregate_data'));
+		}
+
+		/**
+		 * Fetch the data from Google Analytics and store it
+		 */
+		public function aggregate_data(){
+
 		}
 
 	}
