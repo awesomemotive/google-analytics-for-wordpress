568af445cbd21642c6e3eb3a5f514f688f7439eb
Peter van Wilderen <peter@yoast.com>
2014-11-20 13:42:17 +0100

    Correct way to verify for valid metrics

--- admin/dashboards/class-admin-dashboards.php
+++ admin/dashboards/class-admin-dashboards.php
@@ -33,6 +33,20 @@ if ( ! class_exists( 'Yoast_GA_Dashboards' ) ) {
 		public $access_token;
 
 		/**
+		 * Store the active metrics
+		 *
+		 * @var
+		 */
+		public $active_metrics;
+
+		/**
+		 * Store the valid metrics which are available in the Google API
+		 *
+		 * @var array
+		 */
+		private $valid_metrics = array( 'sessions', 'bouncerate' );
+
+		/**
 		 * Store this instance
 		 *
 		 * @var null
@@ -51,19 +65,26 @@ if ( ! class_exists( 'Yoast_GA_Dashboards' ) ) {
 		 */
 		public function init_dashboards() {
 			$Dashboards = array(
-				'sessions'  => array(
-					'title'      => __('Sessions', 'google-analytics-for-wordpress'),
-					'data-label' =>	__('Number of sessions', 'google-analytics-for-wordpress'),
+				'sessions' => array(
+					'title'      => __( 'Sessions', 'google-analytics-for-wordpress' ),
+					'data-label' => __( 'Number of sessions', 'google-analytics-for-wordpress' ),
 				)
 			);
+
+			// Register the active metrics
+			$register = array();
+			foreach( $Dashboards as $metric => $value ){
+				$register[] = $metric;
+			}
+
 			// @TODO enable this after merging to features/dashboards
 			//Yoast_GA_Dashboards_Graph::get_instance()->register($Dashboards);
 
 			$this->data = new Yoast_GA_Dashboards_Data;
 
-			$this->aggregator = new Yoast_GA_Dashboards_Collector;
+			$this->aggregator = new Yoast_GA_Dashboards_Collector( $register );
 
-			$this->register( array('sessions', 'bouncerate') );
+			$this->register( $register );
 		}
 
 		/**
@@ -101,8 +122,10 @@ if ( ! class_exists( 'Yoast_GA_Dashboards' ) ) {
 			}
 
 			if ( is_array( $types ) && count( $types ) >= 1 ) {
-				if ( Yoast_GA_Dashboards_Collector::validate_dashboard_types( $types ) ) {
-					echo 'Register: ' . print_r($types, true);
+				if ( $this->validate_dashboard_types( $types ) ) {
+					$this->active_metrics = $types;
+
+					return true;
 					//return set_transient( 'yst_ga_dashboard_types', $types, 12 * HOUR_IN_SECONDS );
 				}
 			}
@@ -110,6 +133,28 @@ if ( ! class_exists( 'Yoast_GA_Dashboards' ) ) {
 			return false;
 		}
 
+
+
+		/**
+		 * Validate the registered types of dashboards
+		 *
+		 * @param $types
+		 *
+		 * @return bool
+		 */
+		private function validate_dashboard_types( $types ) {
+			$valid = true;
+
+			if ( is_array( $types ) ) {
+				foreach ( $types as $check_type ) {
+					if ( ! in_array( $check_type, $this->valid_metrics ) ) {
+						$valid = false;
+					}
+				}
+			}
+
+			return $valid;
+		}
 	}
 
 }
