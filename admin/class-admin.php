2af730e2f7f70598221648461c530d9a9825ea17
Peter van Wilderen <peter@yoast.com>
2014-11-25 14:23:04 +0100

    Removed obsolete vars from class admin

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -16,20 +16,6 @@ if ( ! class_exists( 'Yoast_GA_Admin' ) ) {
 		 */
 		public $api;
 
-		/**
-		 * Store the API dashboards class
-		 *
-		 * @var
-		 */
-		public $dashboards;
-
-		/**
-		 * Store the API dashboards data class
-		 *
-		 * @var
-		 */
-		public $dashboards_data;
-
 		public function __construct() {
 			parent::__construct();
 
