498dbf873f37d0a47a467089f4d598314a70089f
Peter van Wilderen <peter@yoast.com>
2014-11-28 11:46:00 +0100

    Add parent name in response call for get profiles

--- admin/class-google-analytics.php
+++ admin/class-google-analytics.php
@@ -91,7 +91,7 @@ if ( ! class_exists( 'Yoast_Google_Analytics', false ) ) {
 			$response = $this->do_request( 'https://www.googleapis.com/analytics/v2.4/management/accounts/~all/webproperties/~all/profiles', 'https://www.googleapis.com/auth/analytics.readonly' );
 
 			if ( $response ) {
-				$this->save_profile_response( $response );
+				$this->save_profile_response( $response, $accounts );
 
 				$return = $this->parse_profile_response( $response );
 			}
@@ -238,8 +238,10 @@ if ( ! class_exists( 'Yoast_Google_Analytics', false ) ) {
 		 *
 		 * @param $response
 		 */
-		protected function save_profile_response( $response ) {
-			$this->options['ga_api_response'] = $response;
+		protected function save_profile_response( $response, $accounts ) {
+			$this->options['ga_api_response']          = $response;
+			$this->options['ga_api_response_accounts'] = $accounts;
+
 			$this->update_options();
 		}
 
@@ -273,10 +275,16 @@ if ( ! class_exists( 'Yoast_Google_Analytics', false ) ) {
 					}
 
 					foreach ( $ga_accounts as $key => $ga_account ) {
-						$return[] = array(
+						$tmp_array = array(
 							'id'   => $ga_account['ua'],
 							'name' => $ga_account['title'] . ' (' . $ga_account['ua'] . ')',
 						);
+
+						if ( isset( $this->options['ga_api_response_accounts'][$ga_account['ua']] ) ) {
+							$tmp_array['parent_name'] = $this->options['ga_api_response_accounts'][$ga_account['ua']];
+						}
+
+						$return[] = $tmp_array;
 					}
 				}
 			} catch ( Exception $e ) {
