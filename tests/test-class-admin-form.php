e1161c7eec0cec8a16308663f109f43c42846a32
Peter van Wilderen <peter@yoast.com>
2014-12-18 12:08:15 +0100

    Add test for select input

--- tests/test-class-admin-form.php
+++ tests/test-class-admin-form.php
@@ -45,4 +45,26 @@ class Yoast_GA_Admin_Form_Test extends GA_UnitTestCase {
 		$this->assertEquals( Yoast_GA_Admin_Form::input( 'text' ), $output );
 	}
 
+	/**
+	 * Create a test select field
+	 *
+	 * @covers Yoast_GA_Admin_Form:select()
+	 */
+	public function test_select() {
+		Yoast_GA_Admin_Form::create_form( 'phpunit' );
+
+		$title    = 'Test select';
+		$name     = 'test_select';
+		$values   = array();
+		$values[] = array( 'id' => 1, 'name' => 'PHP Unit' );
+		$values[] = array( 'id' => 2, 'name' => 'Tests' );
+
+		$output = null;
+		$output .= '<div class="ga-form ga-form-input"><label class="ga-form ga-form-select-label ga-form-label-left" id="yoast-ga-form-label-select-phpunit-test_select" />';
+		$output .= 'Test select:</label><select data-placeholder="" name="test_select" id="yoast-ga-form-select-phpunit-test_select">';
+		$output .= '<option value="1" >PHP Unit</option><option value="2" >Tests</option></select></div>';
+
+		$this->assertEquals( Yoast_GA_Admin_Form::select( $title, $name, $values ), $output );
+	}
+
 }
