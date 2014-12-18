368d03b4ef7ae2308b10ca6adad7d1d280f91706
Peter van Wilderen <peter@yoast.com>
2014-12-18 16:41:07 +0100

    Added codestyle in test

--- tests/dashboards/test-class-admin-dashboards-data.php
+++ tests/dashboards/test-class-admin-dashboards-data.php
@@ -7,8 +7,8 @@ class Yoast_GA_Dashboards_Data_Test extends GA_UnitTestCase {
 	 */
 	public function test_set_AND_get_data() {
 		$dataobject = array(
-			'object'	=>	array(
-				'test'	=>	'Value!',
+			'object' => array(
+				'test' => 'Value!',
 			),
 		);
 		$type       = 'phpunit_test_values';
@@ -18,16 +18,16 @@ class Yoast_GA_Dashboards_Data_Test extends GA_UnitTestCase {
 
 		// Set the data object
 		$set = Yoast_GA_Dashboards_Data::set( $type, $dataobject, $start_date, $end_date, $store_as );
-		$this->assertTrue($set);
+		$this->assertTrue( $set );
 
 		// Get the data object
-		$result = Yoast_GA_Dashboards_Data::get( $type );
+		$result   = Yoast_GA_Dashboards_Data::get( $type );
 		$expected = array(
-			'store_as'	=>	$store_as,
-			'type'	=>	$type,
-			'start_date'	=>	$start_date,
-			'end_date'	=>	$end_date,
-			'value'	=>	$dataobject
+			'store_as'   => $store_as,
+			'type'       => $type,
+			'start_date' => $start_date,
+			'end_date'   => $end_date,
+			'value'      => $dataobject
 		);
 
 		$this->assertEquals( $result, $expected );
