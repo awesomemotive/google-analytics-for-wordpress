0cfd320cad9874bb58262c11a06b0e42afec2c7d
Andy Meerwaldt <andy@yoast.com>
2015-01-09 11:30:40 +0100

    Moving notice specific tests to its own file.

---
+++ tests/test-class-google-analytics-notice.php
@@ -0,0 +1,42 @@
+<?php
+
+class Yoast_Google_Analytics_Notice_Test extends GA_UnitTestCase {
+
+	/**
+	 * Test a part of the config warning to make sure we have one
+	 */
+	public function test_config_warning() {
+		ob_start();
+		Yoast_Google_Analytics_Notice::config_warning();
+		$output   = ob_get_clean();
+		$expected = '<div class="error"><p>Please configure your <a href="' . admin_url( 'admin.php?page=yst_ga_settings' ) . '">Google Analytics settings</a>!</p></div>';
+
+		$this->assertEquals( $output, $expected );
+	}
+
+	/**
+	 * Test the warning fetching data to make sure we have one
+	 */
+	public function test_warning_fetching_data() {
+		ob_start();
+		Yoast_Google_Analytics_Notice::warning_fetching_data();
+		$output   = ob_get_clean();
+		$expected = '<div class="error"><p>Failed to fetch the new data from Google Analytics. This might be caused by a problem with the Google service.</p></div>';
+
+		$this->assertEquals( $output, $expected );
+	}
+
+	/**
+	 * Test the warning fetching data to make sure we have one
+	 */
+	public function test_warning_fetching_data_authenticate() {
+		ob_start();
+		Yoast_Google_Analytics_Notice::warning_fetching_data();
+		$output   = ob_get_clean();
+		$expected = '<div class="error"><p>Failed to fetch the new data from Google Analytics. You might need to <a href="' . admin_url( 'admin.php?page=yst_ga_settings' ) . '">reauthenticate</a>.</p></div>';
+
+		$this->assertEquals( $output, $expected );
+	}
+
+
+}
