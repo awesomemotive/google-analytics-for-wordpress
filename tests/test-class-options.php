75a5f4d7729033e8bdf50504ef95200d0a4c45a1
Peter van Wilderen <peter@yoast.com>
2015-01-12 13:51:09 +0100

    Test the new settings functionality

--- tests/test-class-options.php
+++ tests/test-class-options.php
@@ -67,6 +67,28 @@ class Yoast_GA_Options_Test extends GA_UnitTestCase {
 	}
 
 	/**
+	 * Test the checkbox default value (Manual UA code) to bool
+	 *
+	 * @covers Yoast_GA_Options::checkbox_value_to_bool()
+	 */
+	public function test_checkbox_value_to_bool() {
+		$settings_manual_ua = $this->class_instance->checkbox_value_to_bool( 'manual_ua_code' );
+		$this->assertFalse( $settings_manual_ua );
+	}
+
+	/**
+	 * Test if we can update the checkbox and check the new bool
+	 *
+	 * @covers Yoast_GA_Options::checkbox_value_to_bool()
+	 */
+	public function test_checkbox_value_to_bool_AND_update_option() {
+		$this->test_update_option();
+
+		$settings_manual_ua = $this->class_instance->checkbox_value_to_bool( 'manual_ua_code' );
+		$this->assertTrue( $settings_manual_ua );
+	}
+
+	/**
 	 * Test the defaults
 	 *
 	 * @covers Yoast_GA_Options::default_ga_values()
