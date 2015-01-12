41349a798213e8b048539895908a8014e4dcb01c
Andy Meerwaldt <andy@yoast.com>
2015-01-12 10:49:16 +0100

    And updated the tests

--- tests/test-class-google-analytics-notice.php
+++ tests/test-class-google-analytics-notice.php
@@ -21,7 +21,7 @@ class Yoast_Google_Analytics_Notice_Test extends GA_UnitTestCase {
 		ob_start();
 		Yoast_Google_Analytics_Notice::warning_fetching_data();
 		$output   = ob_get_clean();
-		$expected = '<div class="error"><p>Failed to fetch the new data from Google Analytics. This might be caused by a problem with the Google service.</p></div>';
+		$expected = '<div class="error"><p>Data is not up-to-date, there was an error in retrieving the data from Google Analytics. This error could be caused by several issues. If the error persists, please see <a href="http://yoa.st/2p">this page</a>.</p></div>';
 
 		$this->assertEquals( $output, $expected );
 	}
@@ -33,7 +33,7 @@ class Yoast_Google_Analytics_Notice_Test extends GA_UnitTestCase {
 		ob_start();
 		Yoast_Google_Analytics_Notice::warning_fetching_data_authenticate();
 		$output   = ob_get_clean();
-		$expected = '<div class="error"><p>Failed to fetch the new data from Google Analytics. You might need to <a href="' . admin_url( 'admin.php?page=yst_ga_settings' ) . '">reauthenticate</a>.</p></div>';
+		$expected = '<div class="error"><p>It seems the authentication for the plugin has expired, please <a href="' . admin_url( 'admin.php?page=yst_ga_settings' ) . '">re-authenticate</a> with Google Analytics to allow the plugin to fetch data..</p></div>';
 
 		$this->assertEquals( $output, $expected );
 	}
