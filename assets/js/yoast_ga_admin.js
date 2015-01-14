3cb9c52c99219c369d89d7ab4ecdae89255c70ae
Andy Meerwaldt <andy@yoast.com>
2015-01-14 13:15:30 +0100

    When toggling checkbox the authentication token input was showing up. This is fixed.

--- assets/js/yoast_ga_admin.js
+++ assets/js/yoast_ga_admin.js
@@ -1,10 +1,28 @@
 function yst_popupwindow(url, w, h) {
+	'use strict';
 	var left = (screen.width/2)-(w/2);
 	var top = (screen.height/8);
 	return window.open(url, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 }
 
+function yst_ga_switch_manual() {
+	if ( jQuery('#yoast-ga-form-checkbox-settings-manual_ua_code').is(':checked') ) {
+		var is_disabled = true;
+		jQuery('#enter_ua').show();
+		jQuery('#oauth_code').hide();
+	} else {
+		var is_disabled = false;
+		jQuery('#enter_ua').hide();
+		jQuery('#yoast-ga-form-text-settings-manual_ua_code_field').attr('value', '');
+		jQuery('#oauth_code').hide();
+	}
+
+	jQuery('#yoast-ga-form-select-settings-analytics_profile').prop('disabled', is_disabled).trigger('chosen:updated');
+	jQuery('#yst_ga_authenticate').attr('disabled', is_disabled);
+}
+
 jQuery(document).ready(function() {
+	'use strict';
 	jQuery('#ga-tabs').find('a').click(function() {
 		jQuery('#ga-tabs').find('a').removeClass('nav-tab-active');
 		jQuery('.gatab').removeClass('active');
@@ -33,27 +51,12 @@ jQuery(document).ready(function() {
 	jQuery('#' + activeTab).addClass('active');
 	jQuery('#' + activeTab + '-tab').addClass('nav-tab-active');
 
-	function yst_ga_switch_manual() {
-        if ( jQuery('#yoast-ga-form-checkbox-settings-manual_ua_code').is(':checked') ) {
-            jQuery('#enter_ua').show();
-            jQuery("#yoast-ga-form-select-settings-analytics_profile").prop('disabled', true).trigger("chosen:updated");
-            jQuery("#yst_ga_authenticate").attr('disabled', true);
-			jQuery('#oauth_code').hide();
-        } else {
-            jQuery('#enter_ua').hide();
-            jQuery('#yoast-ga-form-text-settings-manual_ua_code_field').attr('value', '');
-            jQuery("#yoast-ga-form-select-settings-analytics_profile").prop('disabled', false).trigger("chosen:updated");
-            jQuery("#yst_ga_authenticate").attr('disabled', false);
-			jQuery('#oauth_code').show();
-        }
-    }
-
 	// Manually enter a UA code
-	jQuery('#yoast-ga-form-checkbox-settings-manual_ua_code').click( function() { yst_ga_switch_manual(); } );
+	jQuery('#yoast-ga-form-checkbox-settings-manual_ua_code').click( yst_ga_switch_manual );
     yst_ga_switch_manual();
 
 	jQuery('#oauth_code').hide();
-	jQuery("#yst_ga_authenticate").click( function() {
+	jQuery('#yst_ga_authenticate').click( function() {
 		jQuery('#oauth_code').show();
 		Focusable.setFocus(jQuery('#oauth_code'), {hideOnESC:true});
 		jQuery('#oauth_code input').focus();
