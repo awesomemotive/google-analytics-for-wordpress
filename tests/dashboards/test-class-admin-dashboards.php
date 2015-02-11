499db5aae75e736b236c983466a1a6c1b66e18c9
Andrey Savchenko <contact@rarst.net>
2015-02-11 21:46:05 +0200

    Moved autoload require in tests to bootstrap file

--- tests/dashboards/test-class-admin-dashboards.php
+++ tests/dashboards/test-class-admin-dashboards.php
@@ -1,7 +1,6 @@
 <?php
 
 // Load dependencies from API-Libs
-require( dirname( __FILE__ ) . '/../../vendor/autoload_52.php' );
 require( dirname( __FILE__ ) . '/../../vendor/yoast/api-libs/google/class-api-google.php' );
 
 class Yoast_GA_Dashboards_Test extends GA_UnitTestCase {
