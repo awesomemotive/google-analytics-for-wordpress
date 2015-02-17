239c2f9dfb0ff4fe75cbc3bf0c89fc91c981176f
Peter van Wilderen <peter@yoast.com>
2015-02-17 16:06:45 +0100

    Dryed interface for building fields

--- admin/class-admin-settings-api.php
+++ admin/class-admin-settings-api.php
@@ -5,6 +5,8 @@
  */
 class Yoast_GA_Admin_Settings_API {
 
+	private $fields = array();
+
 	/**
 	 * Construct the new admin settings api forms
 	 */
@@ -18,41 +20,22 @@ class Yoast_GA_Admin_Settings_API {
 	public function init_yst_ga_settings_general() {
 		register_setting( 'yst_ga_settings_form', 'yst_ga_settings' );
 
-		add_settings_section(
-			'yst_ga_settings_form_section',
-			__( 'General settings', 'google-analytics-for-wordpress' ),
-			null,
-			'yst_ga_settings_form'
-		);
+		$this->add_field( array(
+			'name'        => 'track_outbound',
+			'title'       => __( 'Track outbound click and downloads', 'google-analytics-for-wordpress' ),
+			'description' => 'The help icon description',
+			'value'       => 123,
+			'type'        => 'checkbox',
+		) );
+		$this->add_field( array(
+			'name'        => 'anonymous_data',
+			'title'       => __( 'Allow tracking of anonymous data', 'google-analytics-for-wordpress' ),
+			'description' => 'The help icon description',
+			'value'       => 123,
+			'type'        => 'checkbox',
+		) );
 
-		/**
-		 * Dummy field
-		 */
-		add_settings_field(
-			'yst_ga_text_field_0',
-			__( 'Settings field description', 'test' ),
-			array( $this, 'yst_ga_form_text_field' ),
-			'yst_ga_settings_form',
-			'yst_ga_settings_form_section',
-			array( 'name' => 'name', 'value' => 'value', 'description' => 'Full description with some help' )
-		);
-
-		add_settings_field(
-			'track_outbound',
-			__( 'Track outbound click and downloads', 'google-analytics-for-wordpress' ) . ':',
-			array( $this, 'yst_ga_form_input_field' ),
-			'yst_ga_settings_form',
-			'yst_ga_settings_form_section',
-			array( 'name' => 'name', 'value' => 'value', 'description' => 'Full description with some help' )
-		);
-		add_settings_field(
-			'anonymous_data',
-			__( 'Allow tracking of anonymous data', 'google-analytics-for-wordpress' ) . ':',
-			array( $this, 'yst_ga_form_input_field' ),
-			'yst_ga_settings_form',
-			'yst_ga_settings_form_section',
-			array( 'name' => 'name', 'value' => 'value', 'description' => 'Full description with some help' )
-		);
+		$this->build_settings_section( __( 'General settings', 'google-analytics-for-wordpress' ) );
 	}
 
 	/**
@@ -69,8 +52,8 @@ class Yoast_GA_Admin_Settings_API {
 	 *
 	 * @param $args
 	 */
-	public function yst_ga_form_input_field( $args ) {
-		echo $this->show_help( 'id-' . $args['name'], $args['description'] ) . '<input type="checkbox" name="yst_ga_settings[' . $args['name'] . ']" value="1" ' . checked($args['value'], 1 ) . '>';
+	public function yst_ga_form_checkbox_field( $args ) {
+		echo $this->show_help( 'id-' . $args['name'], $args['description'] ) . '<input type="checkbox" name="yst_ga_settings[' . $args['name'] . ']" value="1" ' . checked( $args['value'], 1 ) . '>';
 	}
 
 	/**
@@ -81,10 +64,44 @@ class Yoast_GA_Admin_Settings_API {
 	 *
 	 * @return string
 	 */
-	public function show_help( $id, $description ) {
+	private function show_help( $id, $description ) {
 		$help = '<img src="' . plugins_url( 'assets/img/question-mark.png', GAWP_FILE ) . '" class="alignleft yoast_help" id="' . esc_attr( $id . 'help' ) . '" alt="' . esc_attr( $description ) . '" />';
 
 		return $help;
+	}
+
+	/**
+	 * Add a field to the fields array
+	 *
+	 * @param $args
+	 */
+	private function add_field( $args ) {
+		$this->fields[] = $args;
+	}
+
+	/**
+	 * Build the settings section from the given fields
+	 *
+	 * @param string $title
+	 */
+	private function build_settings_section( $title ) {
+		add_settings_section(
+			'yst_ga_settings_form_section',
+			$title,
+			null,
+			'yst_ga_settings_form'
+		);
+
+		foreach ( $this->fields as $field ) {
+			add_settings_field(
+				$field['name'],
+				$field['title'] . ':',
+				array( $this, 'yst_ga_form_' . $field['type'] . '_field' ),
+				'yst_ga_settings_form',
+				'yst_ga_settings_form_section',
+				$field
+			);
+		}
 	}
 
 }
