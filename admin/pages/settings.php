ed24f01c11eabdf1d076fee6c31832bae3c4dc30
Andrey Savchenko <contact@rarst.net>
2015-02-23 14:33:00 +0200

    Escaped tracking code output

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -49,7 +49,7 @@ echo Yoast_GA_Admin_Form::create_form( 'settings' );
 				echo '</div>';
 				echo '<div class="ga-form ga-form-input">';
 				echo '<label class="ga-form ga-form-text-label ga-form-label-left" id="yoast-ga-form-label-text-ga-authwithgoogle">' . __( 'Current UA-profile', 'google-analytics-for-wordpress' ) . '</label>';
-				echo $yoast_ga_admin->get_tracking_code();
+				echo esc_html( $yoast_ga_admin->get_tracking_code() );
 				echo '</div>';
 			} else {
 				echo Yoast_GA_Admin_Form::select( __('Analytics profile', 'google-analytics-for-wordpress' ), 'analytics_profile', $profiles, null, false, __( 'Select a profile', 'google-analytics-for-wordpress' ) );
