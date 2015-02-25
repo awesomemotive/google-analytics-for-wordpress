a7072e62694e56b1e0d2eeef4bcca851d8ebc38a
Andrey Savchenko <contact@rarst.net>
2015-02-25 17:12:11 +0200

    Put if on new line for indentation sniff

--- admin/dashboards/views/graph.php
+++ admin/dashboards/views/graph.php
@@ -9,12 +9,14 @@
 		<span class='alignright period'><?php echo __( 'Last month', 'google-analytics-for-wordpress' ); ?></span>
 	</h3>
 
-	<?php if ( empty( $settings['hide_y_axis'] ) ) {
+	<?php
+	if ( empty( $settings['hide_y_axis'] ) ) {
 		echo "<div class='yoast-graph-yaxis'></div >";
 	} ?>
 	<div class="yoast-graph-holder"></div>
 
-	<?php if ( empty( $settings['hide_x_axis'] ) ) {
+	<?php
+	if ( empty( $settings['hide_x_axis'] ) ) {
 		echo "<div class='yoast-graph-xaxis'></div >";
 	} ?>
 </div>
