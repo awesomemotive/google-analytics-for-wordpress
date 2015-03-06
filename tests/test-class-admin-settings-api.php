7cf913fe88e3edd4655fb744ec08581fce2bcb3e
Peter van Wilderen <peter@yoast.com>
2015-03-06 10:05:36 +0100

    Commit uncommitted changes before branch change

---
+++ tests/test-class-admin-settings-api.php
@@ -0,0 +1,38 @@
+<?php
+
+class Yoast_GA_Admin_Form_API_Test extends GA_UnitTestCase {
+
+	/**
+	 * @var
+	 */
+//	private $class_instance;
+//
+//	/**
+//	 * SetUp runs before every test
+//	 */
+//	public function setUp() {
+//		$this->class_instance = new Yoast_GA_Admin_Settings_API();
+//	}
+//
+//	/**
+//	 * @covers Yoast_GA_Admin_Settings_API::init_yst_ga_settings_general
+//	 */
+//	public function test_init_yst_ga_settings_general() {
+//		//do_action('admin_init');
+//
+//		ob_start();
+//
+//		settings_fields( 'yst_ga_settings_form' );
+//		do_settings_sections( 'yst_ga_settings_form_general' );
+//
+//		$result = ob_get_clean();
+//
+//		$this->assertEquals( 'test', $result );
+//
+//	}
+
+	public function test_the_test() {
+		$this->assertTrue( true );
+	}
+
+}
