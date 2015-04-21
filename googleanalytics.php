1c5069170816385e2f31c5e4c23bb770f59bf80d
Joost de Valk <joost@yoast.com>
2015-04-21 15:18:00 +0200

    Fix `@package` tags.

--- googleanalytics.php
+++ googleanalytics.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package    GoogleAnalytics
+ * @package GoogleAnalytics
  * @subpackage Main
  */
 
--- includes/class-options.php
+++ includes/class-options.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * @package GoogleAnalytics
- * @subpackage Includes
+ * @package GoogleAnalytics\Includes
  */
 
 /**
