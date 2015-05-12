598862e52a8f4f86d273bd33a19f4ad748b7c450
Caroline Geven <caroline@yoast.com>
2015-05-12 11:05:59 +0200

    Written tests to check if correct messages are displayed in source when tracking is off for a user

--- tests/test-class-universal.php
+++ tests/test-class-universal.php
@@ -69,11 +69,6 @@ class Universal_Double extends Yoast_GA_Universal {
 	public function get_options_class() {
 		return new Options_Double( $this->test_options );
 	}
-
-	public function do_tracking() {
-		return true;
-	}
-
 }
 
 class Yoast_GA_Universal_Test extends GA_UnitTestCase {
@@ -158,6 +153,9 @@ class Yoast_GA_Universal_Test extends GA_UnitTestCase {
 	 */
 	private $allow_anchor = 0;
 
+	/**
+	 * @var array
+	 */
 	private $ignore_users = array();
 
 	/**
@@ -654,6 +652,7 @@ class Yoast_GA_Universal_Test extends GA_UnitTestCase {
 
 		$user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
 
+		// Login user
 		wp_set_current_user ($user_id);
 
 		$this->go_to( get_permalink( $post_id ) );
@@ -667,29 +666,115 @@ class Yoast_GA_Universal_Test extends GA_UnitTestCase {
 		$output = ob_get_contents();
 		ob_end_clean();
 
-
 		$expected_output  = "<!-- This site uses the Google Analytics by Yoast plugin version " . GAWP_VERSION . " - https://yoast.com/wordpress/plugins/google-analytics/ -->\n";
 		$expected_output .= "<!-- @Webmaster, normally you will find the Google Analytics tracking code here, but the Debug Mode is enabled. To change this, navigate to Analytics -> Settings -> (Tab) Debug Mode and disable Debug Mode to enable tracking of your site.  -->\n";
 		$expected_output .= '<!-- / Google Analytics by Yoast -->';
 
 		$this->assertContains( $expected_output, $output );
 	}
-	
+
+	/**
+	 * When tracking is off for the admin, the tracking code should not be displayed for an admin
+	 *
+	 * @covers Yoast_GA_Universal::tracking()
+	 */
+	public function test_tracking_IS_OFF_FOR_ADMIN_and_user_IS_ADMIN() {
+		$post_id = $this->factory->post->create();
+
+		$user_id = $this->factory->user->create( array( 'ID' => 1, 'role' => 'administrator' ) );
+
+		// Login user
+		wp_set_current_user ($user_id);
+
+		$this->go_to( get_permalink( $post_id ) );
+
+		$this->ignore_users = array( translate_user_role('administrator') );
+
+		$class_instance = new Universal_Double( $this->options() );
+
+		ob_start();
+		$class_instance->tracking();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$expected_output  = "<!-- This site uses the Google Analytics by Yoast plugin version " . GAWP_VERSION . " - https://yoast.com/wordpress/plugins/google-analytics/ -->\n";
+		$expected_output .= "<!-- @Webmaster, normally you will find the Google Analytics tracking code here, but you are in the disabled user groups. To change this, navigate to Analytics -> Settings (Ignore usergroups) -->\n";
+		$expected_output .= '<!-- / Google Analytics by Yoast -->';
+
+		$this->assertContains( $expected_output, $output );
+	}
+
 	/**
 	 * When tracking is off for admin, the tracking code should still be displayed for other users that are not administrator
 	 *
 	 * @covers Yoast_GA_Universal::tracking()
 	 */
-	public function test_tracking_IS_OFF_FOR_ADMIN_and_user_IS_NOT_ADMIN() {
+	public function test_tracking_IS_OFF_FOR_ADMIN_and_user_IS_EDITOR() {
 		$post_id = $this->factory->post->create();
 
-		$user_id = $this->factory->user->create( array( 'role' => 'editor' ) );
+		$user_id = $this->factory->user->create( array( 'ID' => 1, 'role' => 'editor' ) );
 
+		// Login user
 		wp_set_current_user ($user_id);
 
 		$this->go_to( get_permalink( $post_id ) );
 
-		$this->ignore_users = array( 'administrator' );
+		$this->ignore_users = array( translate_user_role('administrator') );
+
+		$class_instance = new Universal_Double( $this->options() );
+
+		ob_start();
+		$class_instance->tracking();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$expected_output  = "_gaTracker('create', '" . $this->manual_ua_code_field . "', 'auto');";
+
+		$this->assertContains( $expected_output, $output );
+	}
+
+	/**
+	 * When tracking is off for editor, the tracking code should not be displayed for an editor
+	 *
+	 * @covers Yoast_GA_Universal::tracking()
+	 */
+	public function test_tracking_IS_OFF_FOR_EDITOR_and_user_IS_EDITOR() {
+		$post_id = $this->factory->post->create();
+
+		$user_id = $this->factory->user->create( array( 'ID' => 1, 'role' => 'editor' ) );
+
+		// Login user
+		wp_set_current_user ($user_id);
+
+		$this->go_to( get_permalink( $post_id ) );
+
+		$this->ignore_users = array( translate_user_role('editor') );
+
+		$class_instance = new Universal_Double( $this->options() );
+
+		ob_start();
+		$class_instance->tracking();
+		$output = ob_get_contents();
+		ob_end_clean();
+
+		$expected_output  = "<!-- This site uses the Google Analytics by Yoast plugin version " . GAWP_VERSION . " - https://yoast.com/wordpress/plugins/google-analytics/ -->\n";
+		$expected_output .= "<!-- Normally you will find the Google Analytics tracking code here, but the webmaster disabled your user group. -->\n";
+		$expected_output .= '<!-- / Google Analytics by Yoast -->';
+
+		$this->assertContains( $expected_output, $output );
+	}
+
+	/**
+	 * When tracking is off for admin, the tracking code should still be displayed for visitors who are not logged in
+	 *
+	 * @covers Yoast_GA_Universal::tracking()
+	 */
+	public function test_tracking_IS_OFF_FOR_ADMIN_and_user_IS_NOT_LOGGED_IN() {
+		$post_id = $this->factory->post->create();
+
+		$this->go_to( get_permalink( $post_id ) );
+
+		$this->ignore_users = array( translate_user_role('administrator') );
 
 		$class_instance = new Universal_Double( $this->options() );
 
